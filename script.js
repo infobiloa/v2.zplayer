879f8456add9d9b0fe02cb5ae50cc403c550c476
infobiloa <109538180+infobiloa@users.noreply.github.com>
2022-10-14 12:13:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getparam(a,e){return e||(e=window.location.href),new URL(e).searchParam
 let s=a=>document.getElementById(a);
 
 function getIdFromUrl(url) { return url.match(/[-\w]{25,}/); }
-let apikey = 'AIzaSyD739-eb6NzS_KbVJq1K8ZAxnrMfkIqPyw';
+let apikey = 'AIzaSyBpiPn10pQmzN4Dwf7-cTCi-c7fsx2NOrE';
 
 let get=()=>{
   getbase();
